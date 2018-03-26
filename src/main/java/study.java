c30b6829696f3f21973f7fdc192d75be0a1fb0f4
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-26 11:57:04 -0500

    save changes

--- src/main/java/study.java
+++ src/main/java/study.java
@@ -1,32 +1,43 @@
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
+//import java.lang.String;
 
 public class study {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Something");
          String input = sc.nextLine();
-         String choice;
-        do {
+//         String choice;
+//        do {
             System.out.println("You Entered: \"" + StringUtils.defaultString(input) + "\"");
-         if (input == input)
-            if (StringUtils.isAlphanumeric(input)) {
-                System.out.println(input + " is not a number: ");
-            } else if (StringUtils.isNumeric(input)){
-                System.out.println(input + "is a number");
+//         if (input == input)
+        String message;
+            if (stringIsANumber(input)) {
+                message = " is a number: ";
+            } else {
+                message= " is not a number";
             }
-            System.out.println("Flipped Case: " + StringUtils.swapCase(input));
-            System.out.println("Reversed: " + StringUtils.reverse(input));
-            System.out.println("Which to continue?");
-            choice = sc.nextLine();
-             if (choice.startsWith("N") || choice.startsWith("n"));
-        } while (choice.startsWith("Y") || choice.startsWith("y"));
-            System.out.println("GoodBye");
-
-
-
+        System.out.println(input + message);
+            System.out.println("Flipped Case: " + flipTheStringCase(input));
+            System.out.println("Reversed: " + isStringReverse(input) );
+//            System.out.println("Which to continue?");
+//            choice = sc.nextLine();
+//             if (choice.startsWith("N") || choice.startsWith("n"));
+//        } while (choice.startsWith("Y") || choice.startsWith("y"));
+//            System.out.println("GoodBye");
     }
 
+    public static boolean stringIsANumber(String input) {
+       return StringUtils.isNumeric(input);
+    }
+
+    public static String flipTheStringCase(String input){
+        return StringUtils.swapCase(input);
+    }
+
+    public static String isStringReverse(String input){
+        return StringUtils.reverse(input);
+    }
 
 }
